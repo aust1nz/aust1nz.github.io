fa0faf7c5de168dfca5a5e963a0fa2fe20e3b03d
Austin Zentz <austin.zentz@gmail.com>
2015-06-16 21:34:00 +0000

    Lazy-load modal images

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,29 @@ if(window.location.hash=="#contact?confirm=true") {
   $('#contact-form').addClass('hidden');
   window.location.hash = "#contact";
 }
+
+// Lazy load modal images
+// from http://bulkan-evcimen.com/lazy_load_bootstrap_carousel_images.html
+$('div.modal').on('shown.bs.modal', function(e){
+  var carousel = $(this).find('.carousel').hide();
+  var loading = $(this).find('.loading');
+  var deferreds = [];
+  var imgs = $('.carousel', this).find('img');
+  // loop over each img    console.log("images");
+  imgs.each(function(){
+    var self = $(this);
+    var datasrc = self.attr('data-src');
+      if (datasrc) {
+        var d = $.Deferred();
+        self.one('load', d.resolve)
+          .attr("src", datasrc)
+          .attr('data-src', '');
+        deferreds.push(d.promise());
+      }
+  });
+  $.when.apply($, deferreds).done(function(){
+    carousel.fadeIn(1000);
+    loading.hide();
+  });
+  
+});
