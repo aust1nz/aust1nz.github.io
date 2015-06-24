ac79917831d73c5c6bef939b7f3a5ace2e795c17
Austin Zentz <austin.zentz@gmail.com>
2015-06-23 21:26:51 -0400

    Refactored form with Ajax.

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,46 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Updates the contact area if the form has been submitted.
-if(window.location.hash=="#contact?confirm=true") {
-  $("#thanks-message").removeClass('hidden');
-  $('#contact-form').addClass('hidden');
-  window.location.hash = "#contact";
-}
+// Validates the contact form and submits it using Formspree.
+$.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+$("#contact-form").validate({
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/austin.zentz@gmail.com", 
+      method: "POST",
+      data: {
+        name: $(form).find("input[name='name']").val(),
+        _replyto: $(form).find("input[name='_replyto']").val(),
+        message: $(form).find("textarea[name='message']").val()
+      },
+      dataType: "json",
+      success: function() {
+        $("#submit-success").fadeIn();
+        $("#contact-form").fadeOut();
+      },
+      error: function() {
+        $("#submit-errors").fadeIn();        
+      }
+    });
+  }
+});
 
 // Lazy load modal images
 // from http://bulkan-evcimen.com/lazy_load_bootstrap_carousel_images.html
